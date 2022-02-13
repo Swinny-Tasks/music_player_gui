e0b6219a3897b0e871b001ca93b323e348c53595
ut-kr <utkarsh@cyberfear.com>
2022-02-13 15:46:07 +1100

    ✨ set up classes

--- backend.rb
+++ backend.rb
@@ -1,1 +1,29 @@
 #!/usr/bin/env ruby
+
+class Album
+    attr_accessor :title, :artist, :genre, :artwork ,:tracks
+    def initialize(title, artist, genre, artwork, tracks)
+        @title = title
+        @artist = artist
+        @artwork = artwork
+        @tracks = tracks
+        @genre = genre
+    end
+end
+
+class Track
+    attr_accessor :title, :path
+    def initialize(title, path)
+        @title = title
+        @path = path
+    end
+end
+
+class Artwork
+    attr_accessor :cover_image, :x_coff, :y_coff
+    def initialize(cover_image, x_coff, y_coff)
+        @cover_image = cover_image
+        @x_coff = x_coff
+        @y_coff = y_coff
+    end
+end
--- main.rb
+++ main.rb
@@ -4,3 +4,19 @@ require './backend.rb'
 require './display.rb'
 
 PATH = "./album.txt" #* hardcoded this since we dont have to prompt user for their input
+
+class Fancy_Player < Gosu::Window
+    def initialize()
+        super 720, 480, false
+        self.caption = "(ง ื▿ ื)ว Gosu Music Player▁ ▂ ▃ ▄"
+        @selected_album = @selected_song = -1
+        @show_menu = [false, false] #? [0] is menu of album list, [1] is menu of track list
+        @music_running = false
+        @album_list = read_album_list(PATH)
+    end
+
+    def needs_cursor?
+        true
+    end
+
+Fancy_Player.new.show()
